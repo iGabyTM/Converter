0cb0f8d4a87f75f140058a15fbd49f7b1a12514b
iGabyTM <contactgabytm@gmail.com>
2022-03-05 12:36:38 +0200

    [ui] fix problem with running the app outside IJ I still have to find a fix for "WARNING: Unsupported JavaFX configuration: classes were loaded from 'unnamed module @<hash>'"

---
+++ ui/src/main/java/me/gabytm/minecraft/converter/ui/App.java
@@ -0,0 +1,22 @@
+package me.gabytm.minecraft.converter.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final var scene = new Scene(new FXMLLoader(getClass().getClassLoader().getResource("scenes/main/scene.fxml")).load());
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("scenes/main/style.css").toExternalForm());
+
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("assets/icon.jpg")));
+        primaryStage.setTitle("Converter 2.0.0");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
--- ui/src/main/java/me/gabytm/minecraft/converter/ui/Main.java
+++ ui/src/main/java/me/gabytm/minecraft/converter/ui/Main.java
@@ -1,22 +1,11 @@
 package me.gabytm.minecraft.converter.ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        final var scene = new Scene(new FXMLLoader(getClass().getClassLoader().getResource("scenes/main/scene.fxml")).load());
-        scene.getStylesheets().add(getClass().getClassLoader().getResource("scenes/main/style.css").toExternalForm());
-
-        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("assets/icon.jpg")));
-        primaryStage.setTitle("Converter 2.0.0");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
     }
 
 }
